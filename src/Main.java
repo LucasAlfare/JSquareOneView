e458f8e152e783f8decc983b4e008c3c36cba46e
Lucas <god-lucas@hotmail.com>
2018-02-16 16:24:55 -0300

    added color to all stickers (needs fix)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import com.main.puzzle.SquareOne;
+
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class Main extends JFrame{
 
@@ -7,7 +11,35 @@ public class Main extends JFrame{
         setLocationRelativeTo(null);
         setDefaultCloseOperation(3);
 
-        SquareOneBonitoDimas squareOneBonitoDimas = new SquareOneBonitoDimas();
+        SquareOneBonitoDimas squareOneBonitoDimas = new SquareOneBonitoDimas(new SquareOne("(0, 2)/(-2, 1)/(0, 3)/(-3, -3)/(0, -3)/(0, -3)/(-1, 0)/(0, -3)/(-2, -1)/(6, -2)/(6, 0)/(0, -5)"));
+
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == 32){
+                    squareOneBonitoDimas.getSquareOne().twist();
+                }
+
+                if (e.getKeyCode() == KeyEvent.VK_F){
+                    squareOneBonitoDimas.getSquareOne().move(true, -1);
+                }
+
+                if (e.getKeyCode() == KeyEvent.VK_J){
+                    squareOneBonitoDimas.getSquareOne().move(true, 1);
+                }
+            }
+        });
+
         add(squareOneBonitoDimas);
 
         setVisible(true);
--- src/SquareOneBonitoDimas.java
+++ src/SquareOneBonitoDimas.java
@@ -1,10 +1,12 @@
+import com.main.puzzle.Piece;
+import com.main.puzzle.SquareOne;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Path2D;
 import java.awt.geom.PathIterator;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 /**
  * ESSA CLASSE É BOA MESMO, AGRADICMENTOS AO MEU PRIMOR EUDES QUE
@@ -14,46 +16,58 @@ public class SquareOneBonitoDimas extends JComponent {
 
     private static final double L = 100;
     private static final double TAN15 = Math.tan(Math.toRadians(15));
+    private static final int TOP_ANCHOR_X = 300;
+    private static final int TOP_ANCHOR_Y = 300;
+    private static final int BOTTOM_ANCHOR_X = 300;
+    private static final int BOTTOM_ANCHOR_Y = TOP_ANCHOR_X + 180;
+
+    private SquareOne squareOne;
+    private ArrayList<Path2D[]> draws;
+
+    public SquareOneBonitoDimas(SquareOne squareOne) {
+        this.squareOne = squareOne;
+        draws = new ArrayList<>();
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         Graphics2D g2d = (Graphics2D) g;
-        //AffineTransform old = g2d.getTransform();
+        AffineTransform old = g2d.getTransform();
 
-        g2d.rotate(Math.toRadians(-30), 300, 300);
-//
-        ArrayList<Path2D[]> pecas = new ArrayList<>(Arrays.asList(
-                meioFull(300, 300),
-                cantoFull(300, 300),
-                meioFull(300, 300),
-                cantoFull(300, 300),
-                meioFull(300, 300),
-                cantoFull(300, 300),
-                meioFull(300, 300),
-                cantoFull(300, 300)));
-        Collections.shuffle(pecas);
+        g2d.rotate(Math.toRadians(-30), TOP_ANCHOR_X, TOP_ANCHOR_Y);
+        drawFace(g2d, TOP_ANCHOR_X, TOP_ANCHOR_Y, true);
+        g2d.setTransform(old);//restaura a rotação original
 
-        boolean previousIsMeio = isMeio(pecas.get(0)[0]);
-        for (Path2D[] fullPeca : pecas){
+        g2d.rotate(Math.toRadians(-30), BOTTOM_ANCHOR_X, BOTTOM_ANCHOR_Y);
+        drawFace(g2d, BOTTOM_ANCHOR_X, BOTTOM_ANCHOR_Y, false);
+
+        //normal...
+    }
+
+    private void drawFace(Graphics2D g2d, int anchorX, int anchorY, boolean face){
+        setupDraws(face, anchorX, anchorY);
+
+        AffineTransform old = g2d.getTransform();
+        //g2d.rotate(Math.toRadians(face ? -30 : 0), anchorX, anchorY);
+
+        boolean prevIsMeio = isMeio(draws.get(0)[0]);
+        for (int i = 0; i < draws.size(); i++) {
             g2d.rotate(
                     Math.toRadians(
-                    previousIsMeio && isMeio(fullPeca[0]) ? 30 : (!previousIsMeio && !isMeio(fullPeca[0]) ? 60 : 45)),
-                    300,
-                    300);
-            previousIsMeio = isMeio(fullPeca[0]);
+                            prevIsMeio && isMeio(draws.get(i)[0]) ? 30 : (!prevIsMeio && !isMeio(draws.get(i)[0]) ? 60 : 45)),
+                    anchorX,
+                    anchorY);
+            prevIsMeio = isMeio(draws.get(i)[0]);
 
-            for (int i = 0; i < fullPeca.length /*canto ou meio*/; i++) {
-                g2d.setColor(Color.green);//i = 0: cor do topo da peça
-                g2d.fill(fullPeca[i]);
+            for (int j = 0; j < squareOne.getPieces(face).get(i).getColors().length; j++) {
+                g2d.setColor(getColorByChar(squareOne.getPieces(face).get(i).getColors()[j]));//i = 0: cor do topo da peça
+                g2d.fill(draws.get(i)[j]);
                 g2d.setColor(Color.black);
-                g2d.draw(fullPeca[i]);
+                g2d.draw(draws.get(i)[j]);
             }
         }
-
-        //g2d.setTransform(old);//restaura a rotação original
-        //normal...
     }
 
     private Path2D[] cantoFull(int x, int y){
@@ -201,5 +215,49 @@ public class SquareOneBonitoDimas extends JComponent {
         }
 
         return numVertices + 1 == 3;
+    }
+
+    private Color getColorByChar(char sigla){
+        switch (sigla){
+            case 'w':
+                return Color.white;
+            case 'y':
+                return Color.yellow;
+            case 'b':
+                return Color.blue;
+            case 'g':
+                return Color.green;
+            case 'r':
+                return Color.red;
+            case 'o':
+                return Color.orange;
+                default:
+                    return Color.cyan;
+        }
+    }
+
+    private void setupDraws(boolean face, int anchorX, int anchorY){
+        draws.clear();
+        for (Piece x : this.getSquareOne().getPieces(face)){
+            if (x.getColors().length == 2){
+                draws.add(meioFull(anchorX, anchorY));
+            } else {
+                draws.add(cantoFull(anchorX, anchorY));
+            }
+        }
+
+        repaint();
+    }
+
+    public static double getL() {
+        return L;
+    }
+
+    public SquareOne getSquareOne() {
+        return squareOne;
+    }
+
+    public void setSquareOne(SquareOne squareOne) {
+        this.squareOne = squareOne;
     }
 }
